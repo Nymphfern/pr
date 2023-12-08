aa82ae5a04a44fa5d20278782c2c8c9aa2c88d1d
Nymphfern <104153368+Nymphfern@users.noreply.github.com>
2023-12-08 17:25:19 +0700

    main.c

--- PR/main.c
+++ PR/main.c
@@ -19,7 +19,7 @@ void call_dataBase(char* command, char* data, char* key)
 	case 0:
 		char* path = "/home/mewtwo/Downloads/PR/dataBase.out\0";
 		execl(path, command, data, key, NULL);
-		//return;
+		
 
 		break;
 	default:
@@ -36,17 +36,22 @@ int main()
 	char data[_SIZE_];
 	char key[_SIZE_];
 
-	//printf("Enter the data: ");
-	//scanf("%s", data);
-
-	//printf("Enter the key: ");
-	//scanf("%s", key);
-
 	if (strcmp(command, "SADD") == 0)
 		{
 			printf("Enter the data: ");
 			scanf("%s", data);
 			strcpy(key, "key");
+		}
+		else if (strcmp(command, "SREM") == 0)
+		{
+			printf("Enter the key: ");
+			scanf("%s", key);
+			strcpy(data, "data");
+		}
+		else if (strcmp(command, "SISMEMBER") == 0)
+		{
+			printf("Enter the key: ");
+			scanf("%s", key);			
 		}
 		else if (strcmp(command, "SPUSH") == 0)
 		{
@@ -54,10 +59,21 @@ int main()
 			scanf("%s", data);
 			strcpy(key, "key");
 		}
+		else if (strcmp(command, "SPOP") == 0)
+		{
+			//printf("%s\n", pop_stack());
+			strcpy(data, "data");
+			strcpy(key, "key");
+		}
 		else if (strcmp(command, "QPUSH") == 0)
 		{
 			printf("Enter the data: ");
 			scanf("%s", data);
+			strcpy(key, "key");
+		}
+		else if (strcmp(command, "QPOP") == 0)
+		{
+			strcpy(data, "data");
 			strcpy(key, "key");
 		}
 		else if (strcmp(command, "HSET") == 0)
@@ -67,27 +83,6 @@ int main()
 
 			printf("Enter the key: ");
 			scanf("%s", key);
-
-		}
-		//------------REMOVE------------
-
-		else if (strcmp(command, "SREM") == 0)
-		{
-			printf("Enter the key: ");
-			scanf("%s", key);
-			strcpy(data, "data");
-		}
-		else if (strcmp(command, "SPOP") == 0)
-		{
-			//printf("%s\n", pop_stack());
-			strcpy(data, "data");
-			strcpy(key, "key");
-		}
-		else if (strcmp(command, "QPOP") == 0)
-		{
-			//printf("%s\n", pop_queue());
-			strcpy(data, "data");
-			strcpy(key, "key");
 		}
 		else if (strcmp(command, "HDEL") == 0)
 		{
@@ -95,24 +90,16 @@ int main()
 			scanf("%s", key);
 			strcpy(data, "data");
 		}
-
-		//------------READ------------
-		else if (strcmp(command, "SISMEMBER") == 0)
-		{
-			printf("Enter the key: ");
-			scanf("%s", key);			
-		}
 		else if (strcmp(command, "HGET") == 0)
 		{
 			printf("Enter the key: ");
 			scanf("%s", key);
-
 		}
 		else
 		{
 			printf("ERROR: incorrect command\n");
 		}
-
+	
 	call_dataBase(command, data, key);
 	
 }
